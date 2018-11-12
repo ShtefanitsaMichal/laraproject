98acf401a1545ebae5a59c65fcc81776b5df7c89
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2018-11-12 14:52:03 +0100

    dashboard created

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>My Site | @yield('title', 'Home Page')</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+            @section('footerScripts')
+        <script src="app.js"> </script>
+            @show
+       
+    </body>
+</html>
