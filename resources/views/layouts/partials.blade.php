6d5e66752c7af18b7b6b5c2f18e1665cc40e65d4
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2018-11-12 22:43:29 +0100

    admin created

---
+++ resources/views/layouts/partials.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/partials/module.blade.php -->
+<div class="sidebar-module">
+<h1>{{ $module->title }}</h1>
+</div>
+<!-- resources/views/partials/module.blade.php -->
+<div class="sidebar-module">
+No modules :(
+</div>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,15 @@
 |
 */
 
+
+Route::group(['prefix'=>'admin', 'namespace'=>'admin', 'middleware'=>['auth']], function(){
+ Route::get('/','DashboardController@dashboard') -> name('admin.index');
+});
+
 Route::get('/', function () {return view('welcome');});
 Route::get('master', function () {return view('layouts/master');});
 Route::get('dashboard', function () {return view('dashboard');});
+Route::get('sidebar', function () {return view('sidebar');});
 Route::get('home', function () {
     $pageName = "home page";
     return view('home')
@@ -23,3 +29,8 @@ Route::get('home', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('passing-data-to-views', function () {
+return view('dashboard')
+->with('key', 'value');
+});
